472987999db949ed146e58b7e8dcc28dc812b09c
Dimonchik <satana2376@yandex.ru>
2015-02-19 19:53:08 +0300

    search

--- www/app/main.php
+++ www/app/main.php
@@ -1,5 +1,6 @@
 <?php
 //main controller
+print_r($_POST);
 //$result['news'] = call("SELECT * FROM `news` WHERE lang='$setlang' ORDER BY `date` DESC LIMIT 0,12 ");
 //$result['adv'] = call("SELECT * FROM `adverts` WHERE lang='$setlang' ORDER BY `date` DESC");
 //$result['img'] = call("SELECT * FROM `main_page` WHERE lang='$setlang' AND pos='img'");
