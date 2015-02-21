f19974458d5cd634320079908f008e4d1c4a8f11
Yuriy <zet9@bk.ru>
2015-02-22 01:30:16 +0300

    user info

--- www/tpl/user.php
+++ www/tpl/user.php
@@ -1,100 +1,101 @@
 				<div class="user-info">
-					<div class="user-lines">
-						<label>картинка</label>
+					<div class="user-avatar">
 						<div><img src="/img/avatar.png"><?=$result['img'];?></img></div>
 					</div>
-					<div class="user-lines">
-						<label>ник</label>
-						<div><?=$result['nic_name']; ?></div>
-					</div>
-					<div class="user-lines">
-						<label>почта</label>
-						<div><?=$result['email'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>имя</label>
-						<div><?=$result['name'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>фамилия</label>
-						<div><?=$result['fname'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>скайп</label>
-						<div><?=$result['skype'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>возраст</label>
-						<div><?=$result['aga'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>титле</label>
-						<div><?=$result['title']; ?></div>
-					</div>
-					<div class="user-lines">
-						<label>рейтинг</label>
-						<div><?=$result['rating'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>часовой пояс</label>
-						<div><?=$result['time'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>дата регистрации</label>
-						<div><?=$result['regdate'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>с какого времени играю</label>
-						<div><?=$result['needtime'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>по какое время играю</label>
-						<div><?=$result['needtimetwo']; ?></div>
-					</div>
-					<div class="user-lines">
-						<label>страна</label>
-						<div><?=$result['strana'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>язык</label>
-						<div><?=$result['lang'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>ранг</label>
-						<div><?=$result['elo'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>сервер</label>
-						<div><?=$result['server'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>роль</label>
-						<div><?=$result['role'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>линия</label>
-						<div><?=$result['lan']; ?></div>
-					</div>
-					<div class="user-lines">
-						<label>кого ищу</label>
-						<div><?=$result['goal'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>я ищу</label>
-						<div><?=$result['I_was_looking_for'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>команда</label>
-						<div><?=$result['team'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>фейсбук</label>
-						<div><?=$result['fc'];?></div>
-					</div>
-					<div class="user-lines">
-						<label>вконтакте</label>
-						<div><?=$result['vk'];?></div>
-					</div>
+					<div class="info-block">
+						<div class="user-lines">
+							<label>ник</label>
+							<div><?=$result['nic_name']; ?></div>
+						</div>
+						<div class="user-lines">
+							<label>почта</label>
+							<div><?=$result['email'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>имя</label>
+							<div><?=$result['name'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>фамилия</label>
+							<div><?=$result['fname'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>скайп</label>
+							<div><?=$result['skype'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>возраст</label>
+							<div><?=$result['aga'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>титле</label>
+							<div><?=$result['title']; ?></div>
+						</div>
+						<div class="user-lines">
+							<label>рейтинг</label>
+							<div><?=$result['rating'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>часовой пояс</label>
+							<div><?=$result['time'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>дата регистрации</label>
+							<div><?=$result['regdate'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>с какого времени играю</label>
+							<div><?=$result['needtime'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>по какое время играю</label>
+							<div><?=$result['needtimetwo']; ?></div>
+						</div>
+						<div class="user-lines">
+							<label>страна</label>
+							<div><?=$result['strana'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>язык</label>
+							<div><?=$result['lang'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>ранг</label>
+							<div><?=$result['elo'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>сервер</label>
+							<div><?=$result['server'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>роль</label>
+							<div><?=$result['role'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>линия</label>
+							<div><?=$result['lan']; ?></div>
+						</div>
+						<div class="user-lines">
+							<label>кого ищу</label>
+							<div><?=$result['goal'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>я ищу</label>
+							<div><?=$result['I_was_looking_for'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>команда</label>
+							<div><?=$result['team'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>фейсбук</label>
+							<div><?=$result['fc'];?></div>
+						</div>
+						<div class="user-lines">
+							<label>вконтакте</label>
+							<div><?=$result['vk'];?></div>
+						</div>
+				</div>
 			</div>
 
  
