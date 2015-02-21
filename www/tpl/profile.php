c5ebbe660919b9433b2a05fda970d89ac54c3ba9
dimtry <satana2387@yandex.ru>
2015-02-22 00:04:37 +0300

    #42

--- www/tpl/profile.php
+++ www/tpl/profile.php
@@ -2,24 +2,25 @@
 	<h1>My page</h1>
 	<div class="mypersonal">
 		<form>
-			<img src="/img/avatar.png">
+		<? if ($result['img']=='') { $scr='avatar.png'; } else $scr=$result['img'];?>
+			<img src="/img/<?=$scr?>">
 			<div class="personal-data">
 				<label>Имя</label>
-				<input name="" type="text" value="<?=$result['nic_name']; ?>">
+				<input name="" type="text" value="<?=$result['name'];?>">
 				<label>Фамилия</label>
 				<input name="" type="text" value="<?=$result['fname'];?>">
 			</div>
 			<div class="location">
 				<label>Возраст</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['aga'];?>">
 				<label>Страна</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['strana'];?>">
 				<label>Язык</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['lang'];?>">
 			</div>
 			<div class="login">
 				<label>Логин</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['nic_name']; ?>">
 			</div>
 			<div class="passwords">
 				<label>Новый пароль</label>
@@ -31,27 +32,91 @@
 			</div>
 			<div class="purpose">
 				<label>Цель</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['fname'];?>">
 			</div>
 			<div class="role">
 				<label>Роль</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['role'];?>">
 			</div>
 			<div class="category">
 				<label>Дивизион</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['elo'];?>">
 			</div>
 			<div class="line">
 				<label>Линия</label>
-				<input name="" type="text">
+				<input name="" type="text" value="<?=$result['lan']; ?>">
 			</div>
 			<div class="line">
-				<label>Часы</label>
-				<input name="" type="text">
+				<label>email</label>
+				<input name="" type="text" value="<?=$result['email'];?>">
 			</div>
+			
+			<div class="line">
+				<label>MMR</label>
+				<input name="" type="text" value="<?=$result['rating'];?>">
+			</div>
+			
+			<div class="line">
+				<label>Часовой пояс</label>
+				<input name="" type="text" value="<?=$result['time'];?>">
+			</div>
+			
+			<div class="line">
+				<label>дата регистрации</label>
+				<input name="" type="text" value="<?=$result['regdate'];?>">
+			</div>
+			
+			<div class="line">
+				<label>с какого времени играю </label>
+				<input name="" type="text" value="<?=$result['needtime'];?>">
+			</div>
+			
+			<div class="line">
+				<label>По какое время играю</label>
+				<input name="" type="text" value="<?=$result['needtimetwo']; ?>">
+			</div>
+			
+			<div class="line">
+				<label>skype</label>
+				<input name="" type="text" value="<?=$result['skype'];?>">
+			</div>
+			
+			
+			
+			<div class="line">
+				<label>сервер</label>
+				<input name="" type="text" value="<?=$result['server'];?>">
+			</div>
+			
+			<div class="line">
+				<label>Цель</label>
+				<input name="" type="text" value="<?=$result['goal'];?>">
+			</div>
+			
+			<div class="line">
+				<label>Ищу</label>
+				<input name="" type="text" value="<?=$result['I_was_looking_for'];?>">
+			</div>
+			
+			<div class="line">
+				<label>команда</label>
+				<input name="" type="text" value="<?=$result['team'];?>">
+			</div>
+			
+			<div class="line">
+				<label>фейсбук</label>
+				<input name="" type="text" value="<?=$result['fc'];?>">
+			</div>
+			
+			<div class="line">
+				<label>фейсбук</label>
+				<input name="" type="text" value="<?=$result['vk'];?>">
+			</div>
+			
+			
 			<div class="desription">
 				<label>Описание</label>
-				<textarea name="">Dimon</textarea>
+				<textarea name=""><?=$result['title']; ?></textarea>
 			</div>
 		</form>
 	</div>
