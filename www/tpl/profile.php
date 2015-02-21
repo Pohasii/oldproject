5e0aa01545b9ade53bac607c874bfe5b17bde047
Dimonchik <satana2376@yandex.ru>
2015-02-21 14:31:03 +0300

    profile

---
+++ www/tpl/profile.php
@@ -0,0 +1,26 @@
+	<div class="tab">
+
+	<div class="user-info">
+	<div><?=$result['nic_name']; ?></div>
+	<div><?=$result['email'];?></div>
+	<div><?=$result['aga'];?></div>
+	<div><?=$result['strana'];?></div>
+	<div><?=$result['role'];?></div>
+	<div><?=$result['title'];?></div>
+	<div><?=date_format(date_create($result['regdate']), 'd.m.y');?></div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+	<div>пуст</div>
+			</div>
+
+	</div>
