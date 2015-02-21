38fa10728fa3434fd568d8c20fee9b489efe9993
Dimonchik <satana2376@yandex.ru>
2015-02-21 18:00:42 +0300

    #22

--- www/tpl/profile.php
+++ www/tpl/profile.php
@@ -6,17 +6,17 @@
 	<div><?=$result['role'];?></div>
 	<div><?=$result['title'];?></div>
 	<div><?=date_format(date_create($result['regdate']), 'd.m.y');?></div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
-	<div>пуст</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
+	<div>РїСѓСЃС‚</div>
 			</div>
