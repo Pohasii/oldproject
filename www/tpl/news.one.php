72a9a246eaba2b9c36956e0fc1d02810a07f9a21
Yuriy <zet9@bk.ru>
2015-02-22 04:30:46 +0300

    more

--- www/tpl/news.one.php
+++ www/tpl/news.one.php
@@ -1,12 +1,16 @@
 <main>
-	<h1>Blabkabla</h1>
-	<img src="/img/avatar.png"></img>
-	<div class="topic-main">
-	<div class="triangle"></div>
-		<div class="topic">
-			Topic
+	<div class="news-title">
+		<div class="title">
+			<h1>Blabkabladfgdfgdfgdfgdfg</h1>
+		</div>
+		<div class="topic-main">
+			<div class="triangle"></div>
+				<div class="topic">
+					Topic
+				</div>
 		</div>
 	</div>
+	<img src="/img/avatar.png"></img>
 	<article>
 		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
