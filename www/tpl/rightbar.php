65c2d1003578639b45713fe06ceab03eb6f2a3b8
Dimonchik <satana2376@yandex.ru>
2015-02-27 14:40:10 +0300

    all full

--- www/tpl/rightbar.php
+++ www/tpl/rightbar.php
@@ -2,8 +2,8 @@
 
 
 <div class="rightbar-top10">	
+ТОП10 - ADC ===
 <?php foreach($rightbarone as $value) { ?>
-
 					<a href="/user/<?=$value['nic_name'];?>">
 						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
 						<div class="rightbar-top10-str" align="center"><?=$value['rating'];?></div>
@@ -14,6 +14,7 @@
 </div>
 
 <div class="rightbar-top10">	
+ТОП10 - MID ===
 <?php foreach($rightbartwo as $value) { ?>
 					<a href="/user/<?=$value['nic_name'];?>">
 						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
@@ -24,6 +25,7 @@
 </div>
 
 <div class="rightbar-top10">	
+ТОП10 - Jungl ===
 <?php foreach($rightbarsecond as $value) { ?>
 					<a href="/user/<?=$value['nic_name'];?>">
 						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
