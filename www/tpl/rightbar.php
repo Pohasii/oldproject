f53d147db04df5776040d8d3d99cc2ee8eec3c08
dimtry <satana2387@yandex.ru>
2015-02-27 17:28:05 +0300

    top edit

--- www/tpl/rightbar.php
+++ www/tpl/rightbar.php
@@ -5,8 +5,10 @@
 ТОП10 - ADC ===
 <?php foreach($rightbarone as $value) { ?>
 					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-str" align="center"><?=$value['rating'];?></div>
+						<div class="rightbar-top10-str" align="center">
+						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+						</div>
 					</a>
 
 				<?php } ?> 
@@ -17,8 +19,10 @@
 ТОП10 - MID ===
 <?php foreach($rightbartwo as $value) { ?>
 					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-str" align="center"><?=$value['rating'];?></div>
+						<div class="rightbar-top10-str" align="center">
+						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+						</div>
 					</a>
 				<?php } ?> 
 
@@ -28,8 +32,10 @@
 ТОП10 - Jungl ===
 <?php foreach($rightbarsecond as $value) { ?>
 					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-str" align="center"><?=$value['rating'];?></div>
+						<div class="rightbar-top10-str" align="center">
+						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+						</div>
 					</a>
 				<?php } ?> 
 
