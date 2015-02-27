336bb2b3466bf71465f3e4edb1d1f9f5337c6c4a
Yuriy <zet9@bk.ru>
2015-02-27 22:40:56 +0400

    fix struc rightbar

--- www/tpl/rightbar.php
+++ www/tpl/rightbar.php
@@ -1,43 +1,39 @@
 <div class="rightbar">	
 
-<div class="rightbar-top10">	
-<div class="rightbar-top10-titl" align=center>ТОП10 - ADC</div>
+	<div class="rightbar-top10">	
+		<div class="rightbar-top10-titl" align=center>ТОП10 - ADC</div>
 
-<?php foreach($rightbarone as $value) { ?>
-					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center">
-						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
-						</div>
-					</a>
+		<?php foreach($rightbarone as $value) { ?>
+			<a href="/user/<?=$value['nic_name'];?>">
+				<div class="rightbar-top10-str" align="center">
+				<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+				<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+				</div>
+			</a>
+		<?php } ?>
+	</div>
 
-				<?php } ?> 
+	<div class="rightbar-top10">	
+		<div class="rightbar-top10-titl" align=center>ТОП10 - MID</div>
+		<?php foreach($rightbartwo as $value) { ?>
+			<a href="/user/<?=$value['nic_name'];?>">
+				<div class="rightbar-top10-str" align="center">
+				<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+				<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+				</div>
+			</a>
+		<?php } ?> 
+	</div>
 
-</div>
-
-<div class="rightbar-top10">	
-<div class="rightbar-top10-titl" align=center>ТОП10 - MID</div>
-<?php foreach($rightbartwo as $value) { ?>
-					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center">
-						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
-						</div>
-					</a>
-				<?php } ?> 
-
-</div>
-
-<div class="rightbar-top10">	
-<div class="rightbar-top10-titl" align=center>ТОП10 - Jungl</div>
-<?php foreach($rightbarsecond as $value) { ?>
-					<a href="/user/<?=$value['nic_name'];?>">
-						<div class="rightbar-top10-str" align="center">
-						<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
-						<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
-						</div>
-					</a>
-				<?php } ?> 
-</div>
-  
+	<div class="rightbar-top10">	
+		<div class="rightbar-top10-titl" align=center>ТОП10 - Jungl</div>
+		<?php foreach($rightbarsecond as $value) { ?>
+			<a href="/user/<?=$value['nic_name'];?>">
+				<div class="rightbar-top10-str" align="center">
+				<div class="rightbar-top10-block" align="center"><?=$value['nic_name']; ?></div>
+				<div class="rightbar-top10-block" align="center"><?=$value['rating'];?></div>
+				</div>
+			</a>
+		<?php } ?> 
+		</div>
 </div>
